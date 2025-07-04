24854b72e58671cda4cddc4faf3cee2bea6c91bc
NuevaAlianza <LASV0901@gmail.com>
2025-07-04 15:37:45 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const SUPABASE_URL = 'https://wrcoawxhvyduinkzwsgc.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyY29hd3hodnlkdWlua3p3c2djIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3MjAxNjksImV4cCI6MjA2NjI5NjE2OX0.ktA1A3nlTadvAZIMakQ8NVu5BVHHKzwKPevfAm6kkf4';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhenVyZmJtcm5zYWRrcnh1dHBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2NTY0NzEsImV4cCI6MjA2NzIzMjQ3MX0.GmBk4XRX0gVhw20gTGX8jM58mb2gXOFC7qkY8iMEU9k';
 
 // ✅ El objeto global es `supabase`, ya definido por el SDK UMD
 window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
