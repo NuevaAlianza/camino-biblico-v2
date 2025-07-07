6486683014ed0066eb4d8129984036c6bc4d675b
NuevaAlianza <LASV0901@gmail.com>
2025-07-07 18:21:41 -0400

    Update rpg.js

--- js/rpg.js
+++ js/rpg.js
@@ -92,6 +92,13 @@ async function mostrarStatsBienvenida() {
     </div>
   `;
 }
+function normalizar(str) {
+  return (str || "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim();
+}
 
 
 // --- 3. Supabase: cargar y guardar progreso ---
